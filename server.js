3a908a23cc778990dbabedccc22e7f5019f266dd
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-02-26 11:59:13 +0100

    HTTP petitions

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 
+const router = express.Router();
 const app = express();
 
-app.use('/', (req, res) => {
-  console.group('Root');
-  res.send('Hi!');
-  console.groupEnd('Root');
+app.use(router);
+
+router.get('/message', (req, res) => {
+  res.send('Messages list');
 })
+
+router.post('/message', (req, res) => {
+  res.send('Message added');
+})
+
+// app.use('/', (req, res) => {
+//   console.group('Root');
+//   res.send('Hi!');
+//   console.groupEnd('Root');
+// })
 
 app.listen(3000, () => console.log('App listening on port 3000...'));
