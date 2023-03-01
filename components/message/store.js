2017d943e83f4dd3b8a9f0af489a1ebf1113af08
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-03-01 21:09:38 +0100

    store.js removed logs

--- components/message/store.js
+++ components/message/store.js
@@ -4,20 +4,12 @@ const list = [];
 
 (async function loadDB() {
   await fs.readFile('../../store.txt', (err, data) => {
-    console.group('Load DB');
-    
     if (err)
       return;
 
     const dataString = data.toString();
-    console.log(dataString);
     const parsedData = JSON.parse(dataString);
-    console.log(parsedData);
-    
     parsedData.forEach( msg => list.push(msg) );
-    console.log(list);
-    
-    console.groupEnd('Load DB');
   })
 })();
 
